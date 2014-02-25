bf137e6a2bedcc9ec6f5b932efc66fdd8bb14556
Leonardo Rossetto <leonardoxh@gmail.com>
2014-02-24 22:07:27 -0300

    copyright

--- CustomFont/lib/src/main/java/com/github/leonardoxh/customfont/FontButton.java
+++ CustomFont/lib/src/main/java/com/github/leonardoxh/customfont/FontButton.java
@@ -1,3 +1,18 @@
+/*
+* Copyright 2013-2014 Leonardo Rossetto
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package com.github.leonardoxh.customfont;
 
 import android.content.Context;
@@ -15,12 +30,12 @@ public class FontButton extends Button {
 
     public FontButton(Context context, AttributeSet attrs) {
         super(context, attrs);
-        this.setCustomFont(context, attrs);
+        setCustomFont(context, attrs);
     }
 
     public FontButton(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        this.setCustomFont(context, attrs);
+        setCustomFont(context, attrs);
     }
 
     protected void setCustomFont(Context context, AttributeSet attrs) {
